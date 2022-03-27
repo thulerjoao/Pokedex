fc88fd10c06ba0933ecf13cfdd429cea211a4941
thulerjoao <thuler_lima@hotmail.com>
2022-03-27 20:40:52 -0300

    commit v1.6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const req = require('express/lib/request');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
